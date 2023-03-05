a88893fb70367d6f6861a1cf63d63330b9709332
nunomourinho <56609351+nunomourinho@users.noreply.github.com>
2023-03-05 15:07:56 +0000

    Delete repeated folder

--- main/forensicVM/bin/mount-vbox.sh
+++
@@ -1,2 +0,0 @@
-mount -t vboxsf forensic-images /forensicVM/mnt/forensic-images
-mount -t vboxsf vm /forensicVM/mnt/vm
--- main/forensicVM/bin/script_reset_activation_XP.sh
+++
@@ -1,5 +0,0 @@
-/forensicVM/bin/remove-hibernation.sh $1
-rand_name=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 7;`
-cp /forensicVM/scripts/reset_ActivationXP.bat /tmp/reset_ActivationXP$rand_name.bat
-virt-customize -a $1 -firstboot /tmp/reset_ActivationXP$rand_name.bat
-rm /tmp/reset_ActivationXP$rand_name.bat

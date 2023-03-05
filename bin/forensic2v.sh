3172a42410a88c7ae95334507303218e77719f67
nunomourinho <56609351+nunomourinho@users.noreply.github.com>
2023-03-05 15:26:55 +0000

    serveral code fixes

--- bin/forensic2v.sh
+++ bin/forensic2v.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Image is the complete path for the forensic image
+
 if [[ -z $1 ]] || [[ -z $2 ]]; then
     echo "Sintaxe: forensic2v <forensic-image> <name> [copy|snap]"
     exit 1
 fi
 
+imagemanager=""
 if [[ -n $1 ]]; then
-    ewfinfo $1
-    if [[ $? -eq 0 ]]; then
+    # shellcheck disable=SC1073
+    # shellcheck disable=SC1072
+    if [[ ewfinfo "$1" -eq 0 ]]; then
         imagemanager="ewf"
     else
-	affinfo $1
-        if [[ $? -eq 0 ]]; then
+        if [[ affinfo "$1" -eq 0 ]]; then
            imagemanager="aff"
         else
-            qemu-img info $1
-            if [[ $? -eq 0 ]]; then
+            if [[ qemu-img info "$1" -eq 0 ]]; then
                imagemanager="qemu"
             else
                echo "Image format not detected. Aborting"
@@ -35,6 +36,7 @@ image=$1
 
 # Name: Name
 name=$2
+# shellcheck disable=SC2236
 if [ ! -z "$3" ]; then
 	mode=$3
 else
@@ -51,74 +53,79 @@ if [ $mode == "snap" ]; then
    touch /tmp/qemu-img-cp-now
 fi
 
-mkdir $vm_name
-mkdir $image_ewf_mnt
-mkdir $win_mount
+mkdir "$vm_name"
+mkdir "$image_ewf_mnt"
+mkdir "$win_mount"
 
 tput bold
 tput setaf 2
 echo "1) Access forensic image location"
 tput sgr0
-###mount -o username=qemu,pass=qemu,nobrl -t cifs //192.168.1.75/convertidos /mnt/destino
 tput bold
 tput setaf 2
 echo "2) Mount forensic image"
 tput sgr0
-ewfmount "$image" $image_ewf_mnt/
+# run command if mode == "snap"
+if [ $imagemanager == "ewf" ]; then
+   ewfmount "$image" "$image_ewf_mnt"/
+fi
+
 tput bold
 tput setaf 2
 echo "3) Get image information"
 tput sgr0
-virt-inspector $image_ewf_mnt/ewf1 | tee $vm_mount/info-$name.txt
+virt-inspector "$image_ewf_mnt"/ewf1 | tee $vm_mount/info-"$name".txt
 tput bold
 tput setaf 2
 echo "4) Create backing file snapshot"
 tput sgr0
-cd $vm_name
-qemu-img create -f qcow2 -b $image_ewf_mnt/ewf1 -F raw S0001-P0000-$name.qcow2-sda
+# shellcheck disable=SC2164
+cd "$vm_name"
+qemu-img create -f qcow2 -b "$image_ewf_mnt"/ewf1 -F raw S0001-P0000-"$name".qcow2-sda
 tput bold
 tput setaf 2
 echo "5) Activate nbd block device"
 tput sgr0
 /sbin/modprobe nbd max_parts=25
-qemu-nbd --connect=/dev/nbd0 S0001-P0000-$name.qcow2-sda
+qemu-nbd --connect=/dev/nbd0 S0001-P0000-"$name".qcow2-sda
 tput bold
 tput setaf 2
 echo "6) Remove hibernate file"
 tput sgr0
-xmllint --xpath '//mountpoint' $vm_mount/info-$name.txt | awk -F'"' '{print $2}' | while read line ; do
-   NUMBER=$(echo $line | tr -dc '0-9')
+# shellcheck disable=SC2162
+xmllint --xpath '//mountpoint' $vm_mount/info-"$name".txt | awk -F'"' '{print $2}' | while read line ; do
+   NUMBER=$(echo "$line" | tr -dc '0-9')
    tput bold
    tput setaf 2
-   echo "6.1) Removing hibernate file form partion number $NUMBER"
+   echo "6.1) Removing hibernate file form partition number $NUMBER"
    tput sgr0
-   ntfsfix /dev/nbd0p$NUMBER
+   ntfsfix "/dev/nbd0p$NUMBER"
    sync
-   ntfsfix -d /dev/nbd0p$NUMBER
+   ntfsfix -d "/dev/nbd0p$NUMBER"
    sync
-   mount -t ntfs-3g /dev/nbd0p$NUMBER $win_mount -o remove_hiberfile
-   umount  $win_mount
-   ntfsfix /dev/nbd0p$NUMBER
+   mount -t ntfs-3g "/dev/nbd0p$NUMBER" "$win_mount" -o remove_hiberfile
+   umount  "$win_mount"
+   ntfsfix "/dev/nbd0p$NUMBER"
    sync
-   ntfsfix -d /dev/nbd0p$NUMBER
+   ntfsfix -d "/dev/nbd0p$NUMBER"
 done
-qemu-nbd --disconnect /dev/nbd0
+qemu-nbd --disconnect "/dev/nbd0"
 sync
 tput bold
 tput setaf 2
 echo "7) Add virtio drivers and qemu guest"
 tput sgr0
-virt-v2v -i disk $vm_name/S0001-P0000-$name.qcow2-sda  -o qemu -of qcow2 -os $vm_name -on S0002-P0001-$name.qcow2
+virt-v2v -i disk "$vm_name/S0001-P0000-$name.qcow2-sda"  -o qemu -of qcow2 -os "$vm_name" -on "S0002-P0001-$name.qcow2"
 if [ $mode != "snap" ]; then
   tput bold
   tput setaf 2
   echo "8) Umounting paths"
   tput sgr0
   cd ..
-  umount  $image_ewf_mnt
+  umount  "$image_ewf_mnt"
   tput bold
   tput setaf 2
   echo "9) Delete temp snapshot"
   tput sgr0
-  rm $tmp_mount/snapshot-temp-$name.qcow2.snap
+  rm "$tmp_mount/snapshot-temp-$name.qcow2.snap"
 fi

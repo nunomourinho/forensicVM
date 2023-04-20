4b4c122d23edd0e5498b17a16e7decae50580d15
Nuno Mourinho <nuno.mourinho@gmail.com>
2023-04-20 22:41:53 +0100

    run or convert - changes

--- bin/run-or-convert.sh
+++ bin/run-or-convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "$@"
+
 # Show help if no arguments or if -h or --help are specified
 if [[ $# -eq 0 ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
     echo "Usage: $0 --windows-share <value> --share-login <value> --share-password <value> --replacement-share <value> --forensic-image-path <value> --folder-uuid <value> --copy [copy|snap]"
@@ -48,6 +50,11 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    --share-port)
+    share_port="$2"
+    shift # past argument
+    shift # past value
+    ;;
     *)    # unknown option
     echo "Unknown option: $1"
     exit 1
@@ -56,36 +63,28 @@ esac
 done
 
 
+mkdir /forensicVM/mnt/vm/$folder_uuid/mnt
 # Mount the windows share
-mount -t cifs "$windows_share" /mnt -o username="$share_login",password="$share_password"
+mount -o username=$share_login,pass=$share_password,nobrl,ro,port=$share_port -t cifs //127.0.0.1/$windows_share /forensicVM/mnt/vm/$folder_uuid/mnt
 
 # Run forensic2v script
-/forensivVM/bin/forensic2v.sh "$forensic_image_path" "$folder_uuid" "$copy"
+/forensicVM/bin/forensic2v.sh "/forensicVM/mnt/vm/$folder_uuid/mnt/$forensic_image_path" "$folder_uuid" "$copy"
 
-# Unmount the windows share
-umount /mnt
 
-# Replace the share with the replacement share
-sed -i "s|$windows_share|$replacement_share|g" "$folder_uuid.vmdk"
 
-pwd
-cd /forensicVM
-pwd
-cd bin
-pwd
 
 # Test if file exists
-if ! [ -e "/forensicVM/bin/forensic2v.sh" ]
-then
-  echo "File does not exist. Running commands..."
-  # Run your commands here
-  echo "Done."
-else
-  sudo ./forensic2v.sh
-  echo "The forensicVM already exists. Mounting folder if necessary"
-  if mount | grep -q "/forensicVM/mnt/vm/directory/mnt"; then
-    echo "/forensicVM/mnt/vm/directory/mnt is mounted"
-  else
-    echo "/forensicVM/mnt/vm/directory/mnt is not mounted"
-  fi
-fi
+#if ! [ -e "/forensicVM/bin/forensic2v.sh" ]
+#then
+#  echo "File does not exist. Running commands..."
+# Run your commands here
+#  echo "Done."
+#else
+#  sudo ./forensic2v.sh
+#  echo "The forensicVM already exists. Mounting folder if necessary"
+#  if mount | grep -q "/forensicVM/mnt/vm/directory/mnt"; then
+#    echo "/forensicVM/mnt/vm/directory/mnt is mounted"
+#  else
+#    echo "/forensicVM/mnt/vm/directory/mnt is not mounted"
+#  fi
+#fi

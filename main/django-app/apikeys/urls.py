f22ff959f2cf8446358c90f27efb252cc705e7d9
Nuno Mourinho <nuno.mourinho@gmail.com>
2023-05-02 05:37:28 +0100

    runscriptview - runs a generic script as a webservice --- testing. Do not use in production. This is like a remote webshell with root access. From the security perspective.... a very bad idea....

--- main/django-app/apikeys/urls.py
+++ main/django-app/apikeys/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProtectedView
+from .views import ProtectedView, RunScriptView
 
 urlpatterns = [
     path('test/', ProtectedView.as_view(), name='test'),
+    path('run-script/', RunScriptView.as_view(), name='run_script'),
 ]
 
--- main/django-app/apikeys/views.py
+++ main/django-app/apikeys/views.py
@@ -1,3 +1,4 @@
+import subprocess
 from django.shortcuts import render
 
 # Create your views here.
@@ -22,3 +23,29 @@ class ProtectedView(APIView):
         else:
             return Response({'error': 'API key required'}, status=status.HTTP_401_UNAUTHORIZED)
 
+
+
+class RunScriptView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        api_key = request.META.get('HTTP_X_API_KEY')
+        if api_key:
+            try:
+                api_key = ApiKey.objects.get(key=api_key)
+            except ApiKey.DoesNotExist:
+                return Response({'error': 'Invalid API key'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({'error': 'API key required'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if 'script' not in request.data:
+            return Response({'error': 'Missing script parameter'}, status=status.HTTP_400_BAD_REQUEST)
+
+        script = request.data['script']
+
+        try:
+            result = subprocess.run(script, shell=True, capture_output=True)
+            return Response({'output': result.stdout.decode('utf-8'), 'error_code': result.returncode}, status=status.HTTP_200_OK)
+        except subprocess.CalledProcessError as e:
+            return Response({'error': e.output.decode('utf-8'), 'error_code': e.returncode}, status=status.HTTP_400_BAD_REQUEST)

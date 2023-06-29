0169d17e7fbae5ed78f47f9f15c6adf5ce842378
Nuno Mourinho <nuno.mourinho@gmail.com>
2023-06-29 23:36:46 +0100

    Fixed record and stop functions

--- main/django-app/apikeys/views.py
+++ main/django-app/apikeys/views.py
@@ -53,10 +53,8 @@ async def create_video(uuid):
     socket_path = f"/forensicVM/mnt/vm/{uuid}/run/qmp.sock"
     frames_path = f"/forensicVM/mnt/vm/{uuid}/frames/"
 
-    print(f"{uuid} inside create_video")
     if not os.path.exists(frames_path):
         os.makedirs(frames_path)
-        print("created dirs in path")
 
     existing_frames = sorted(glob.glob(f"{frames_path}/fr*.ppm"))
     next_frame_number = len(existing_frames) + 1
@@ -65,7 +63,6 @@ async def create_video(uuid):
     try:
         await qmp.connect(socket_path)
         res = await qmp.execute('screendump', {"filename": next_frame_path})
-        print(f"Frame saved: {next_frame_path}")
         return next_frame_number
     except Exception as e:
         print(e)
@@ -73,11 +70,12 @@ async def create_video(uuid):
         await qmp.disconnect()
 
     # check if recording is stopped for this VM
-    if uuid in recordings and not recordings[uuid]:
-        scheduler.remove_job(f'create_video_job_{uuid}')
-        print("removed job")
+    print(recordings)
+    if uuid in recordings:
+        if not recordings[uuid]:
+            scheduler.remove_job(f'create_video_job_{uuid}')
+            del recordings[uuid]
 
-scheduler = AsyncIOScheduler()
 
 @method_decorator(csrf_exempt, name='dispatch')
 class RecordVideoVMView(View):
@@ -96,21 +94,46 @@ class RecordVideoVMView(View):
             return JsonResponse({'error': f'VM with UUID {uuid} not found'}, status=status.HTTP_404_NOT_FOUND)
 
         print(uuid)
+        record = False
         if uuid not in recordings:
+            record = True
+            print("uuid not in the recordigs")
+        elif uuid in recordings and not recordings[uuid]:
+            record = True
+            print("uuid is in the recordings, but not in the uuid")
+
+
+        if record:
+            output_video_path = f"/forensicVM/mnt/vm/{uuid}/video.mp4"
+            frames_path = f"/forensicVM/mnt/vm/{uuid}/frames/"
+            video_path = f"/forensicVM/mnt/vm/{uuid}/video/"
+            shutil.rmtree(frames_path)  # Delete the directory and its contents
+            if not os.path.exists(frames_path):
+                os.makedirs(frames_path)  # Recreate the empty directory
+            if not os.path.exists(video_path):
+                os.makedirs(video_path)  # Recreate the empty directory
             recordings[uuid] = True
             print('started schedule recording')
-            scheduler.add_job(create_video, 'interval', seconds=1, id=f'create_video_job_{uuid}', args=[uuid], replace_existing=True)
+            scheduler = AsyncIOScheduler()
+            scheduler.add_job(create_video, 'interval', seconds=0.04, id=f'create_video_job_{uuid}', args=[uuid], replace_existing=True)
             scheduler.start()
-            # Sleep for 1 hour or until the stop event is set
-            await asyncio.sleep(60)
+            for _ in range(3600):  # run the loop for 3600 interactions (one hour)
+                await asyncio.sleep(1)  # sleep for 1 second
+                if not recordings[uuid]:  # if recordings[uuid] is False, break the loop
+                    break
+
             scheduler.remove_job(f'create_video_job_{uuid}')
 
             # use ffmpeg to convert the sequence of frames into a video
-            frames_path = f"/forensicVM/mnt/vm/{uuid}/frames/"
-            output_video_path = f"/forensicVM/mnt/vm/{uuid}/video.mp4"
-            ffmpeg_command = f"ffmpeg -r 1 -i {frames_path}/fr%05d.ppm -vcodec libx264 -pix_fmt yuv420p {output_video_path}"
+            ffmpeg_command = f"ffmpeg -y -r 25 -i {frames_path}/fr%05d.ppm -vcodec libx264 -pix_fmt yuv420p {output_video_path}"
             os.system(ffmpeg_command)
-            result = {'video_recorded': True, 'message': f'Video recorded for VM with UUID {uuid}'}
+            # Move the video file to the video_path directory with sequential renaming
+            video_count = len(os.listdir(video_path))
+            new_video_path = os.path.join(video_path, f"video{video_count + 1:04d}.mp4")
+            shutil.move(output_video_path, new_video_path)
+
+            result = {'video_recorded': True, 'message': f'Video recorded with the name video{video_count + 1:04d}.mp4'}
+            recordings[uuid] = False
             return JsonResponse(result, status=status.HTTP_200_OK)
         else:
             return JsonResponse({'error': f'Recording for VM with UUID {uuid} has already started'}, status=status.HTTP_400_BAD_REQUEST)
@@ -136,34 +159,6 @@ class RecordVideoVMView(View):
         return user, None
 
 
-#
-#@method_decorator(csrf_exempt, name='dispatch')
-#class RecordVideoVMView(View):
-#    authentication_classes = [SessionAuthentication]
-#    permission_classes = []
-#
-#    async def post(self, request, uuid):
-#        user, api_key_error = await sync_to_async(self.get_user_or_key_error)(request)
-#        if api_key_error:
-#            return api_key_error
-#
-#        vm_path = f"/forensicVM/mnt/vm/{uuid}"
-#        vm_exists = await sync_to_async(os.path.exists)(vm_path)
-#
-#        if not vm_exists:
-#            return JsonResponse({'error': f'VM with UUID {uuid} not found'}, status=status.HTTP_404_NOT_FOUND)
-#
-#        if uuid not in recordings:
-#            recordings[uuid] = True
-#            scheduler.add_job(create_video, 'interval', seconds=1, id=f'create_video_job_{uuid}', replace_existing=True)
-#        else:
-#            return HttpResponseBadRequest(f'Recording for VM with UUID {uuid} has already started')
-#
-#        result = {'video_recording_started': True, 'message': f'Video recording started for VM with UUID {uuid}'}
-#
-#        return JsonResponse(result, status=status.HTTP_200_OK)
-#
-
 @method_decorator(csrf_exempt, name='dispatch')
 class StopVideoRecordingVMView(View):
     authentication_classes = [SessionAuthentication]
@@ -175,9 +170,12 @@ class StopVideoRecordingVMView(View):
             return api_key_error
 
         if uuid in recordings:
+            print("DEBUG: Stop uuid in recordings")
             recordings[uuid] = False
         else:
             return HttpResponseBadRequest(f'No recording to stop for VM with UUID {uuid}')
+            recordings[uuid] = False
+
 
         result = {'video_recording_stopped': True, 'message': f'Video recording stopped for VM with UUID {uuid}'}
 

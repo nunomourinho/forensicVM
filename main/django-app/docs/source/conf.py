e48b4d56c197957c42bffc08ecbed3699859f9ee
Nuno Mourinho <nuno.mourinho@gmail.com>
2023-07-07 00:31:16 +0100

    Added https://sphinx-rtd-theme.readthedocs.io/en/stable/installing.html (theme)

--- main/django-app/docs/source/conf.py
+++ main/django-app/docs/source/conf.py
@@ -15,7 +15,7 @@ release = '1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 #extensions = []
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc','sphinx_rtd_theme']
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -24,7 +24,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 import os

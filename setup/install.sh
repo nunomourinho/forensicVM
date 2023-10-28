d0642ba072afcb06023682c770c8a4f327171378
Nuno Mourinho <nuno.mourinho@gmail.com>
2023-10-28 13:51:04 +0100

    update install

--- setup/install.sh
+++ setup/install.sh
@@ -1,6 +1,6 @@
 cd /
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-yes | git clone --recurse-submodules https://github.com/nunomourinho/forensicVM.git
+yes | git clone --recurse-submodules https://github.com/nunomourinho/forensicVM.git /
 
 
 cp /forensicVM/etc/systemd/system/forensicvm /etc/systemd/system/forensicvm

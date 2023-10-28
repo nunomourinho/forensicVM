316b8a826d8099fffe66e17d571d53678f5d0dc1
Nuno Mourinho <nuno.mourinho@gmail.com>
2023-10-28 13:59:58 +0100

    update install

--- setup/install.sh
+++ setup/install.sh
@@ -1,6 +1,7 @@
 cd /
 yes | git clone --recurse-submodules https://github.com/nunomourinho/forensicVM.git
 cd /forensicVM/setup/apps
+apt install -y dselect
 bash restore.sh
 
 cp /forensicVM/etc/systemd/system/forensicvm /etc/systemd/system/forensicvm
